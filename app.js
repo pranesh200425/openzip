ae792f0ca6d406b67c9f4f83738d7a1d46d95f06
pranesh <pranesh200425@gmail.com>
2025-05-24 13:52:37 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const zip = document.getElementById("fileIn");
+const filecon = document.getElementById("filecon")
+
+zip.addEventListener("change", async (event) => {
+
+  const file = event.target.files[0];
+  const zip = await JSZip.loadAsync(file);
+  console.log(zip);
+  Object.keys(zip.files).forEach(
+    async function(filename) {
+      const a = document.createElement("a")
+      //const h4 = document.createElement("h4");
+      a.classList.add('downloadlink');
+      a.textContent = filename;
+
+      const zipEntry = zip.files[filename];
+
+      if (zipEntry.dir) return;
+
+      const blob = await zipEntry.async("blob");
+
+      a.href = URL.createObjectURL(blob);
+      a.download = filename
+
+      const name = document.createElement("div");
+      name.classList.add('name');
+      name.append(a);
+      const downloadCon = document.createElement('div');
+      downloadCon.classList.add('downloadCon')
+      downloadCon.append(name);
+      filecon.append(downloadCon)
+
+
+    }
+  )
+
+})
+
+
